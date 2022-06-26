a6c60bbb078d25376450823982b149535f13fe33
Yaniv <yaniv208@gmail.com>
2022-06-26 22:53:40 +0300

    Added several exceptions

--- src/main/java/restapi/webapp/exceptions/advices/GlobalAdvice.java
+++ src/main/java/restapi/webapp/exceptions/advices/GlobalAdvice.java
@@ -6,14 +6,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import restapi.webapp.exceptions.UserNotFoundException;
+import restapi.webapp.exceptions.UsersNotFoundException;
 
 @ControllerAdvice
-
 public class GlobalAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     String userNotFoundHandler(UserNotFoundException unfe){
+        return unfe.getMessage();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UsersNotFoundException.class)
+    String usersNotFoundHandler(UsersNotFoundException unfe){
         return unfe.getMessage();
     }
 }
