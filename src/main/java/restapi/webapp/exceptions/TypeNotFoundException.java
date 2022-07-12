85a9b7fe1bfd60b034ca4adf26590d397f5f0d5e
Ram Michaeli <ram153486@gmail.com>
2022-07-12 18:53:34 +0300

    * Fixed :"should throw exception: this name is not available instead of "500 error"

--- src/main/java/restapi/webapp/exceptions/TypeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package restapi.webapp.exceptions;
-
-public class TypeNotFoundException extends RuntimeException{
-    public TypeNotFoundException(String type) {
-        super("Type " + type + " is not valid.");
-    }
-}
